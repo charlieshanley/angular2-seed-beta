f1a79526012090bac8540579c6a1a44d2a8c7e1a
CHanley <CHanley@Mathematica-mpr.com>
2017-06-17 17:58:50 -0400

    took Mosh's code

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,71 +1,12 @@
 import {Component} from 'angular2/core';
-import {CoursesComponent} from './courses.component';
-import {AuthorsComponent} from './authors.component';
-import {FavstarComponent} from './favstar.component';
-import {LikeComponent} from './like.component';
-import {VoterComponent} from './voter.component';
-import {ZippyComponent} from './zippy.component';
-import {ContactFormComponent} from './contact-form.component';
-import {SubscriptionFormComponent} from './subscription-form.component';
+import {SignUpFormComponent} from './signup-form.component'
 
 @Component({
     selector: 'my-app',
+    directives: [SignUpFormComponent],
     template: `
-    <voter
-        [myVote]="post.myVote"
-        [voteCount]="post.voteCount"
-        (vote)="onVote($event)">
-    </voter>
-    <likebutton
-        [isLiked]="post.isLiked"
-        [nLikes]="post.nLikes"
-        (like-change)="onLikeChange($event)">
-    </likebutton>
-
-    <favstar
-        [isFavorite]="post.isFavorite"
-        (favorite-change)="onFavoriteChange($event)">
-    </favstar>
-
-    <zippy [title]="'Diospyros virginiana'">
-        Compared with the kaki persimmon, the American persimmon is smaller and of richer flavor.
-    </zippy>
-    <zippy [title]="'Mespilus germanica'">
-        The medlar was once a common garden fruit in Europe, but is not commonly grown today.
-    </zippy>
-
-    <contact-form></contact-form>
-
-    <subscription-form></subscription-form>
-    `,
-    directives: [
-        VoterComponent,
-        LikeComponent,
-        FavstarComponent,
-        ZippyComponent,
-        ContactFormComponent,
-        SubscriptionFormComponent
-    ]
+        <signup-form></signup-form>
+    `
 })
 export class AppComponent {
-    post = {
-        title: "Title",
-        isFavorite: true,
-        isLiked: false,
-        nLikes: 6,
-        myVote: 0,
-        voteCount: 8
-    }
-
-    onFavoriteChange($event){
-        console.log($event);
-    }
-    onLikeChange($event){
-        this.post.isLiked = $event.isLiked;
-        this.post.nLikes = $event.nLikes;
-    }
-    onVote($event){
-        this.post.myVote = $event.myVote;
-        this.post.voteCount = $event.voteCount;
-    }
 }
