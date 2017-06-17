c2dcd8a374533c943881a72d4f0b162e1f04e33d
CHanley <CHanley@Mathematica-mpr.com>
2017-06-17 18:59:35 -0400

    model driven forms lesson

--- app/signup-form.component.ts
+++ app/signup-form.component.ts
@@ -1,8 +1,25 @@
 import {Component} from 'angular2/core';
+import {ControlGroup, Validators, FormBuilder} from 'angular2/common';
+import {UsernameValidators} from './usernameValidators.ts';
 
 @Component({
     selector: 'signup-form',
     templateUrl: 'app/signup-form.component.html'
 })
 export class SignUpFormComponent {
+    form: ControlGroup;
+
+    constructor(fb: FormBuilder){
+        this.form = fb.group({
+            username: ['', Validators.compose([
+                Validators.required,
+                UsernameValidators.cannotContainSpace
+            ]), UsernameValidators.shouldBeUnique],
+            password: ['', Validators.required]
+        });
+    }
+
+    signup(){
+        console.log(this.form.value);
+    }
 }
